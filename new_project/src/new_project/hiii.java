eb4cd053032004e73c8c26a8de573fe794e6b134
User <User@192.168.0.202>
2023-02-22 19:29:36 +0530

    f

--- new_project/src/new_project/hiii.java
+++ new_project/src/new_project/hiii.java
@@ -5,7 +5,7 @@ public class hiii {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helooo");
-		System.out.println("dhidasdisakdosad,sal");
+		System.out.println("dhidasdisakdosad,sval");
 
 	}
 
